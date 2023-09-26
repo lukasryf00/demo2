bdb7a316f304c445fdb9938fa2c26ef0cc9114da
lukas.ryf00 <lukas.ryf00@gmail.com>
2023-09-26 14:51:15 +0200

    asdf

---
+++ src/test/java/ch/zhaw/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
